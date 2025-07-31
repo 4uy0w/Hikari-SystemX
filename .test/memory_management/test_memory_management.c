2ea825e619898111dba6c9ac53d83bcf788a1c20
Hikari <silentyamato10@gmail.com>
2025-07-31 12:43:38 +0700

    Melakukan full test untuk alokasi memori dari 2 objek

--- .test/memory_management/test_memory_management.c
+++ .test/memory_management/test_memory_management.c
@@ -2,20 +2,58 @@
 
 #include <stdio.h>
 
+struct __position{
+    float x,y,z;
+};
+
+struct __human{
+    char *name;
+    int age,power,money;
+    float height,weight,speed;
+    struct __position * position;
+};
+
+typedef struct __human human_t;
+
+human_t * create_human(char *human_name,int human_age,int human_power,int human_money,float human_height,float human_weight,float human_speed);
+void destroy_human(human_t * human_ptr);
+
 int main(int argc,char *argv[])
 {
-    char *test_alloc = (char *) __malloc(1000);
-    char *str_test = "Hello world!";
-    int index = 0;
-
-    while(*str_test != '\0'){
-        test_alloc[index++] = *str_test++;
+    human_t * Aldebaran = create_human("Aldebaran",15,100,10000,180.5,50.1,10.5);
+    human_t * Sabian = create_human("Sabian",16,100,10000,180.5,60.5,100.4);
+    
+    if(Aldebaran != (human_t *) NULL){
+        printf("Success to create human with name Aldebaran! \n");
     }
-    test_alloc[index] = '\0';
+    if(Sabian != (human_t *) NULL){
+        printf("Success to create human with name Sabian! \n");
+    }
 
-    printf("%s \n",test_alloc);
-
-    __free(test_alloc);
+    destroy_human(Aldebaran);
+    destroy_human(Sabian);
 
     return 0;
+}
+
+human_t * create_human(char *human_name,int human_age,int human_power,int human_money,float human_height,float human_weight,float human_speed)
+{
+    human_t *new_human = (human_t *) __malloc(sizeof(human_t));
+
+    new_human->name = human_name;
+    new_human->age = human_age;
+    new_human->power = human_power;
+    new_human->money = human_money;
+    new_human->height = human_height;
+    new_human->weight = human_weight;
+    new_human->speed = human_speed;
+    new_human->position = (struct __position *) NULL;
+
+    return (new_human != (human_t *) -1 ? new_human : (human_t *) NULL);
+}
+void destroy_human(human_t * human_ptr)
+{
+    if(__free(human_ptr) == (void *) -1){
+        printf("failed to dealocated human! \n");
+    }
 }
